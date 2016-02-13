8d41906a4827ec0110021e2e68a51ef633073993
Igor Kamenev <igorkamenev@yandex.ru>
2016-02-13 22:07:57 +0300

    Initial Commit

---
+++ WindyModules/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WindyModules
+//
+//  Created by Igor Kamenev on 13/02/16.
+//  Copyright © 2016 Igor Kamenev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
